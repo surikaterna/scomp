5d0a90251e95e3a92e72c211bf0ace71dfa415b0
spralle <sprawl@onlajn.nu>
2017-03-05 23:11:44 +0100

    ADDED DEP

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+import {Logger} from 'slf';
+const LOG = Logger.getLogger('scomp:core')
+
 export class Scomp {
   constructor(wire) {
-    this._wire = wire || new NullWire();
+    this._wire = wire;
   }
 
   request(req) {
     this._wire(req);
+  }
+
+  client() {
+    LOG.info('client builder');
   }
 }
--- test/scomp.js
+++ test/scomp.js
@@ -10,13 +10,14 @@ describe('Scomp', () => {
   it('should do something', (done) => {
     // Scomp.request('viewdb');
     // Creates a proxy
-    const viewdb = Scomp.client('viewdb');
+    const viewdb = new Scomp().client('viewdb');
     //viewdb.query('shipments').then();
-    viewdb.observe('shipments', {}).next((ev) => {
+   /* viewdb.observe('shipments', {}).next((ev) => {
       //_process(ev);
       done();
     }).error((err) => {
       done(err);
     });
+    */
   });
 });
