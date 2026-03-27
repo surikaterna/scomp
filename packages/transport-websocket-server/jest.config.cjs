9c729ace92dcba6c48605656fe2b596acc9ac378
spralle <sprawl@onlajn.nu>
2026-03-28 00:08:55 +0100

    feat: add websocket client/server transports with integration tests

---
+++ packages/transport-websocket-server/jest.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
+  testMatch: ['**/*.spec.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['babel-jest', {
+      babelrc: false,
+      configFile: false,
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        '@babel/preset-typescript'
+      ]
+    }]
+  },
+  moduleNameMapper: {
+    '^@scomp/core$': '<rootDir>/../core/src',
+    '^@scomp/transport-websocket-client$': '<rootDir>/../transport-websocket-client/src'
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  modulePathIgnorePatterns: ['<rootDir>/dist']
+};
